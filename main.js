85ab0692b7977a4d63a37456f94c0fe232fe7ee9
Yebin Kim <dpqls1227@gmail.com>
2024-02-13 01:12:28 +0900

    game upgrade

--- main.js
+++ main.js
@@ -9,67 +9,95 @@
 //유자가 이미 입력한 숫자를 또 입력하면, 알려준다. 기회가 차감되지 않는다
 
 let computerNum = 0
-//document.getElementById()
-// document : 웹 페이지 그 자체
-// getElementById : html태그를 가져오는데 id를 통해 가져온다
 let playButton = document.getElementById("play-button")
 let userInput = document.getElementById("user-input")
-let resultArea = document.getElementById("result-area")
-let resetButton = document.getElementById("reset-button")
+let resultText = document.querySelector(".result-text")
+let resetButton = document.querySelector(".button-reset")
+let resultAreaImg = document.querySelector(".main-img");
+// 추가문제1. 남은 기회를 5번이 아닌 10번으로 바꾸고 싶다면? 어디를 바꾸면 될까?
+// let chances = 10
 let chances = 5
 let gameOver = false
 let chanceArea = document.getElementById("chance-area")
+//유저가 이미 입력한 값들을 저장하는 배열
+let history = []
 
-//playButton태그에 event를 추가해주는데
-//그건 'click' event이고 클릭했을 때 실행될 함수는
-//'play'야
-//'play'이렇게 작성한 코드의 뜻은 함수를 매개변수로 넘기겠다는 의미인데(함수도 매개변수로 넘길수 있다!!)
-//'play()'이렇게 작성하면 코드가 바로 실행되기 때문에 'click' event가 실행될때만
-//해당 함수가 동작하게 하려면 play만 작성해서 매개변수로 넘겨주어야 한다
 playButton.addEventListener("click",play)
 resetButton.addEventListener("click", reset)
+// 추가문제5. input창에 포커스를 두면 내가 그전에 입력한 값이 자동으로 지워지게 하려면 어떻게 해야할까?
+// userInput.addEventListener("focus", focus)
+userInput.addEventListener("focus",function(){userInput.value = ""})
+
 
 function pickRandomNumber() {
-  //Math.random() => 0 ~ 1 범위의 랜덤숫자를 생성해주는 함수
-  //Math.round() => 반올림
-  //Math.floor() => 내림/버림
-  //그런데 Math.random()은 1과 가까운 숫자까지 반환하지 1을 포함하진 않음 그래서 최종적으로 1을 더해주어야 함
   computerNum = Math.floor(Math.random()*100) + 1
+  
+  // computerNum를 1~10사이의 랜덤번호를 받고싶다면?
+  // 추가문제4. 랜덤변수 생성의 범위를 1~10으로 변경
+  // computerNum = Math.floor(Math.random()*10) + 1
   console.log('정답', computerNum)
 }
 
 function play() {
   let userValue = userInput.value
 
+  //입력 범위 유효성 검사
+  if(userValue < 1 || userValue > 100) {
+    // console.log('1 ~ 100 사이의 숫자만 입력해주세요')
+    resultText.textContent = "1 ~ 100 사이의 숫자만 입력해주세요"
+    return
+  }
+
+    //이미 입력한 값 유효성 검사
+    // for(let i=0; history.length > i; i++) {
+    //   if(userValue == history[i]) {
+    //     resultArea.textContent = "이미 입력한 숫자입니다. 다시 입력해주세요"
+    //     return;
+    //   }
+    // }
+  
+    if(history.includes(userValue)) {
+      resultText.textContent = "이미 입력한 숫자입니다. 다시 입력해주세요"
+      return;
+    }
+
   //기회 차감
   chances--;
-  // ""는 정적인 값에만 사용가능
-  // 정적인 값과 동적인 값을 같이 사용하고 싶을 때는
-  // ``(백틱)을 사용해 주어야 한다
-  // ${chances} : 동적인값 chances 사용
   chanceArea.textContent = `남은 찬스 : ${chances}번`
   console.log('chances', chances)
 
   if(userValue > computerNum) {
-    //textContent
-    // resultArea태그의 값을 ""값(지정한 값)으로 바꿔준다
-    resultArea.textContent = "DOWN!!!"
-    // console.log("Down!!!")
+    resultAreaImg.src = "https://media.giphy.com/media/r2puuhrnjG7vy/giphy.gif"
+    resultText.textContent = "DOWN!!!"
   } else if(userValue < computerNum) {
-    resultArea.textContent = "UP!!!"
-    // console.log("Up!!!")
+    resultAreaImg.src = "https://media0.giphy.com/media/3ov9jExd1Qbwecoqsg/200.gif"
+    resultText.textContent = "UP!!!"
   } 
   else {
   // else if(userValue == computerNum) {
-    resultArea.textContent = "맞췄습니다!!!"
-    // console.log('정답입니다!!!')
+    resultText.textContent = "맞췄습니다!!!"
+    resultAreaImg.src = "https://media.tenor.com/images/0a81b89954678ebe228e15e35044f7a5/tenor.gif"
+    //내가 생각한 코드
+    // playButton.disabled = true
+
+    //선생님 코드 -> 이미 gameOver = true이면 playButton이 disabled되는 로직이 짜여있기 때문에
+    //맞췄을 때 gameOver = true로 바꿔주는 방법도 있음
+    gameOver = true
   }
+
+  //유저가 이미 입력한 값을 배열에 저장
+  history.push(userValue)
+  console.log('history', history)
 
   if(chances < 1) {
     gameOver = true;
   }
 
-  if(gameOver == true) {
+
+  //추가문제3. if(gameover == true)를 if(gameover)로 해도 똑같이 동작이 될까? 동작이 된다면 왤까?
+	// 동작한다
+	// 왜냐하면 gameover값에 true값을 지정했으니 그 변수의 값이 곧 true이니까?
+  if(gameOver) {
     playButton.disabled = true
   }
 
@@ -77,12 +105,24 @@ function play() {
 
 function reset() {
   //user input창이 깨끗하게 정리되고 
-  // userInput.value = null;
   userInput.value = "";
   //새로운 번호가 생성되고
   pickRandomNumber()
+  resultAreaImg.src =
+  "https://media1.giphy.com/media/9DinPR8bzFsmf74j9W/giphy.gif";
+  resultText.textContent = "결과값이 여기 나옵니다"
+  chances = 5
+  chanceArea.textContent = `남은 찬스 : ${chances}번`
+  playButton.disabled = false
+  userValueList = []
+}
 
-  resultArea.textContent = "결과값이 여기 나옵니다"
+// 추가문제5. input창에 포커스를 두면 내가 그전에 입력한 값이 자동으로 지워지게 하려면 어떻게 해야할까?
+
+//=> userInput만 잠시 사용하고 말 함수이기 때문에 익명함수로 처리
+// 함수 선언 시 메모리 소모됨
+function focus() {
+  userInput.value = "";
 }
 
 //랜덤번호 생성
